86efc8db633f3cfd6cc3054fd1a55acaa2831558
RDUSRD <urdaneta816@gmail.com>
2022-08-22 16:42:52 -0400

    codigo001

---
+++ Imprimir_001.rb
@@ -0,0 +1,7 @@
+#Practicando Imprimir los primeros codigos
+
+#Con este comando puts, se imprime como un print, pero al final tiene un \n automatico
+puts "Hola mundo"
+
+#El tipico print como en python
+print "Hola mundo"
