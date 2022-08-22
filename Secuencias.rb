c94748c8324752cfcdc501327ccc2585afa695bc
RDUSRD <urdaneta816@gmail.com>
2022-08-22 19:03:27 -0400

    Hasta arrays

--- Secuencias.rb
+++ Secuencias.rb
@@ -35,3 +35,15 @@
     #numeric found on https://ruby-doc.org/core-2.5.0/Numeric.html
     #math methods on https://ruby-doc.org/core-2.5.1/Math.html
 
+#Input method
+    #in order to get something from the user, we use gets
+    print "Coloque un numero:"
+    mat = gets #This method will get the input and will agregate an enter space at the end always
+    print ("El numero es " + mat + ", bien hecho" )
+
+    #To mitigate that space we use .chomp() method
+    mat = gets.chomp() #Making the input without an enter space at the end
+    print ("El numero es " + mat + ", bien hecho" )
+
+#Arrays methods
+    #found on https://ruby-doc.org/core-3.1.0/Array.html
