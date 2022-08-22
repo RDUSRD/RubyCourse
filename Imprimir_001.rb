74d6442f9bb823cb29c98d75d010a37a68f4a92e
RDUSRD <urdaneta816@gmail.com>
2022-08-22 18:29:32 -0400

    Curso hasta numerico

--- Imprimir_001.rb
+++
@@ -1,7 +0,0 @@
-#Practicando Imprimir los primeros codigos
-
-#Con este comando puts, se imprime como un print, pero al final tiene un \n automatico
-puts "Hola mundo"
-
-#El tipico print como en python
-print "Hola mundo"
---
+++ Secuencias.rb
@@ -0,0 +1,37 @@
+#Practicando Imprimir los primeros codigos
+
+    #Con este comando puts, se imprime como un print, pero al final tiene un \n automatico
+    puts "Hola mundo"
+
+    #El tipico print como en python
+    print "Hola mundo"
+
+    #Se concatenan strings
+    puts ("Hola "+"mundo")
+
+
+#Variables
+
+    #Se crea una variable normal
+    variable1 = "Hola "
+
+    #Usamos la varibale
+    puts ( variable1 +"mundo")
+
+#Tipos de datos en ruby
+    #String
+    #Integer
+    #Decimal
+    #Booleano
+    #nil = null
+
+#string methods
+    #String methods documentation found in https://www.rubyguides.com/2018/01/ruby-string-methods/
+
+
+
+#numeric methods
+    #found on https://ruby-doc.org/core-2.5.0/Integer.html
+    #numeric found on https://ruby-doc.org/core-2.5.0/Numeric.html
+    #math methods on https://ruby-doc.org/core-2.5.1/Math.html
+
