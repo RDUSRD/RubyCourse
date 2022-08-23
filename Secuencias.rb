417e9a86aec8e068428599669833a5635129b670
RDUSRD <urdaneta816@gmail.com>
2022-08-23 10:37:24 -0400

    Until handling errors

--- Secuencias.rb
+++ Secuencias.rb
@@ -71,7 +71,29 @@
 #Control of expressions statement
     #Found on https://docs.ruby-lang.org/en/2.4.0/syntax/control_expressions_rdoc.html
     #this means the if, loops and more in ruby
-#
+#File class
+    When you want to manipulate files you use the class File that have already some methods that can help to use files
+    found on https://docs.ruby-lang.org/en/2.4.0/syntax/control_expressions_rdoc.html
 
+    there is to ways to open or use a file
+    -the classic: using a block o code where you open first the file and then sotre in a variable
+    -the other: using a variable definition first and the set the value of a open file
+    the different between then is that the classic close th file at the end of the block automatically
+    in the other hand, the other method dont close the file so you have to close it manually at the end to
+    ensure the variable dont get more space in the program
+
+    when you use a command line for handling files, it can works like reading first line, and the next code after reading the first
+    line, will read the second one because the first was already readed.
+
+    all the modes to manipulate files https://www.geeksforgeeks.org/file-handling-in-ruby/
+
+
+#Handling errors
+    found on https://www.geeksforgeeks.org/ruby-exception-handling/#:~:text=In%20Ruby%2C%20exception%20handling%20is,flow%20of%20the%20program's%20instructions.
+
+    When we handling errors, we use "rescue" after a begin block to catch any error that can happen in the program
+
+    also, when we are handling errors, we should find the especific error to not catch every error in ruby and be more
+    specific when you are attending to say to user what error he have.
 
 =end
